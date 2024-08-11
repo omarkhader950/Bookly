66d42aabecf8194e47e6016900afaa7632ea9f6c
Omarz <omarkhader950@gmail.com>
2024-08-11 17:23:39 +0300

    first commit

---
+++ lib/features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:equatable/equatable.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
+}
---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,12 @@
+import 'package:bookly/features/splash/presentation/views/widgets/Splash_View_Body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SplashViewBody(),);
+  }
+
+}
